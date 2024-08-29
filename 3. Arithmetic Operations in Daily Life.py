46091f92cf4ca1aeded6e00d67c8a5c39e28ccc8
Andy Huang <huangandy54@gmail.com>
2024-08-28 21:01:46 -0700

    reup

---
+++ 3. Arithmetic Operations in Daily Life.py
@@ -0,0 +1,16 @@
+# Objective:
+# The aim of this assignment is to get familiarized with basic arithmetic operations and understand how they can be applied in everyday situations.
+
+# Task 1: Grocery Store Math
+# Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic
+
+bread, peanut_butter, jelly=5.99,4.99, 4.50
+total_cost=bread+peanut_butter+jelly
+print(total_cost)
+
+# Task 2: Bank Interest
+# If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the total amount in your account after a year. So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. For the example the expected outcome would be $10,700.
+savings_account,interest_rate=10000,.07
+end_of_year_amount=savings_account+(savings_account*interest_rate)
+print(end_of_year_amount)
+
